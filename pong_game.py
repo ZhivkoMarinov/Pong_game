7afeaeb89ee17334e7f92a25bad3c771139a7b77
ZhivkoMarinov <79455283+ZhivkoMarinov@users.noreply.github.com>
2021-12-20 18:46:29 +0200

    Add files via upload

---
+++ pong_game.py
@@ -0,0 +1,105 @@
+import turtle
+import time
+
+window = turtle.Screen()
+window.title('My game')
+window.setup(width=800, height=600)
+window.bgcolor('black')
+window.tracer(0)
+
+# paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape('square')
+paddle_a.color('white')
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape('square')
+paddle_b.color('white')
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape('square')
+ball.color('white')
+ball.penup()
+ball.goto(0, 0)
+
+# Values in pixels. Configure by CPU speed
+ball.dx = 0.03
+ball.dy = -0.03
+
+
+# Function
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+
+# Keyboard biding
+window.listen()
+window.onkeypress(paddle_a_up, 'w')
+window.onkeypress(paddle_a_down, 's')
+window.onkeypress(paddle_b_up, 'Up')
+window.onkeypress(paddle_b_down, 'Down')
+
+
+# main loop
+while True:
+    window.update()
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border check
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    # ball hitting the paddle
+    # print(paddle_b.ycor(), int(ball.ycor()), int(ball.xcor()))
+    if int(ball.xcor()) == 330 and int(ball.ycor()) in range(int(paddle_b.ycor() - 55), int(paddle_b.ycor() + 55)):
+        ball.sety(ball.ycor())
+        ball.dx *= -1
