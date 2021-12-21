697de259359e941ae485a34eec61725f29cd31fb
ZhivkoMarinov <79455283+ZhivkoMarinov@users.noreply.github.com>
2021-12-21 02:51:45 +0200

    Add files via upload

--- pong_game.py
+++ pong_game.py
@@ -2,7 +2,7 @@ import turtle
 import time
 
 window = turtle.Screen()
-window.title('My game')
+window.title('Lamav Pong')
 window.setup(width=800, height=600)
 window.bgcolor('black')
 window.tracer(0)
@@ -36,8 +36,8 @@ ball.penup()
 ball.goto(0, 0)
 
 # Values in pixels. Configure by CPU speed
-ball.dx = 0.03
-ball.dy = -0.03
+ball.dx = 0.2
+ball.dy = -0.2
 
 
 # Function
@@ -72,11 +72,34 @@ window.onkeypress(paddle_a_down, 's')
 window.onkeypress(paddle_b_up, 'Up')
 window.onkeypress(paddle_b_down, 'Down')
 
+# Result
+goals_a = 0
+goals_b = 0
 
+
+result_a = turtle.Turtle()
+result_a.speed(0)
+result_a.color('white')
+result_a.penup()
+result_a.goto(-380, 270)
+result_a.write(0, align='left', font=('Arial', 16, 'bold'))
+result_a.hideturtle()
+
+
+result_b = turtle.Turtle()
+result_b.speed(0)
+result_b.color('white')
+result_b.penup()
+result_b.goto(380, 270)
+result_b.write(0, move=False, align='right', font=('Arial', 16, 'bold'))
+result_b.hideturtle()
+
+
+print(goals_a)
 # main loop
 while True:
-    window.update()
-
+    window.update()    
+   
     # Move the ball
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
@@ -92,18 +115,29 @@ while True:
 
     if ball.xcor() > 390:
         ball.goto(0, 0)
-        ball.dx *= -1
+        ball.dx *= -1        
+        goals_a += 1
+        result_a.clear()
+        result_a.write(goals_a, align='left', font=('Arial', 16, 'bold'))
+        
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
-
+        goals_b += 1
+        result_b.clear()
+        result_b.write(goals_b, move=False, align='right', font=('Arial', 16, 'bold'))
+        
+        
     # ball hitting the paddle
     # print(paddle_b.ycor(), int(ball.ycor()), int(ball.xcor()))
-    if int(ball.xcor()) == 330 and int(ball.ycor()) in range(int(paddle_b.ycor() - 55), int(paddle_b.ycor() + 55)):
+    if int(ball.xcor()) == 330 and int(ball.ycor()) in range(int(paddle_b.ycor() - 55), int(paddle_b.ycor() + 56)):
         ball.sety(ball.ycor())
         ball.dx *= -1
 
-    if int(ball.xcor()) == -330 and int(ball.ycor()) in range(int(paddle_a.ycor() - 55), int(paddle_a.ycor() + 55)):
+    if int(ball.xcor()) == -330 and int(ball.ycor()) in range(int(paddle_a.ycor() - 55), int(paddle_a.ycor() + 56)):
         ball.sety(ball.ycor())
-        ball.dx *= -1
+        ball.dx *= -1   
+    
+    
+    
